257128a0d19d48605c656442979f3390892f6855
alejandro giraldo <alejandro@giraldopineda.com>
2023-08-19 18:22:53 -0500

    feature: basic settings and publish panel.
    - Settings panel
    - Publish panel
    - Prompt Input
    - Ajax callback
    - Publish button
    
    With this feature we are now able to create posts interacting with chatGPT, and using simple text field to send an specific topic for the blog post.

--- pricode-chatgpt.php
+++ pricode-chatgpt.php
@@ -16,7 +16,7 @@ require 'vendor/autoload.php';
 
 
 function pricode_chatgpt_init(){
-    $yourApiKey = '';
+    $yourApiKey = get_option('pricode_chatgpt_api_key');
     return $client = OpenAI::client($yourApiKey);    
 }
 
@@ -40,8 +40,8 @@ function pricode_chatgpt_get_content( $prompt ){
     return $response;
 }
 
-function pricode_chatgpt_create_post(){
-    $prompt = 'Hi, please tell me how to cook carbonara pasta, but please make a blog entry for this topic, in a json format { title, content: [ { heading, [paragraphs] } ] }';
+function pricode_chatgpt_create_post( $prompt ){
+    
     $content = pricode_chatgpt_get_content( $prompt );
     $blog_response = json_decode($content->choices[0]->message->content);
     
@@ -73,7 +73,113 @@ function pricode_chatgpt_create_post(){
       'post_status'   => 'publish',
       'post_author'   => 1
   );
-    wp_insert_post( $new_post );
+    return wp_insert_post( $new_post );
+
     
 }
 
+
+function pricode_chatgpt_menus() {
+
+    //Settings page
+    add_menu_page(
+        __( 'Pricode ChatGPT', 'pricode-chatgpt' ),
+        'Pricode ChatGPT',
+        'manage_options',
+        'pricode-chatgpt-settings',
+        'pricode_chatgpt_settings_callback'
+    );
+
+    //Submenu pages
+    add_submenu_page(
+        'pricode-chatgpt-settings',
+        __( 'Publish Random Post', 'pricode-chatgpt' ),
+        __( 'Publish Random Post', 'pricode-chatgpt' ),
+        'manage_options',
+        'pricode-chatgpt-publish-post',
+        'pricode_chatgpt_publish_post_callback'
+    );
+
+    //Register settings
+    add_action( 'admin_init', 'pricode_chatgpt_register_settings' );
+
+}
+add_action( 'admin_menu', 'pricode_chatgpt_menus' );
+
+function pricode_chatgpt_register_settings(){
+
+    //register our settings
+    register_setting( 'pricode-chatgpt-settings-group', 'pricode_chatgpt_api_key' );
+}
+
+function pricode_chatgpt_settings_callback(){
+?>
+    <h1>Settings</h1>
+    <form method="post" action="options.php">
+        <?php settings_fields( 'pricode-chatgpt-settings-group' ); ?>
+        <?php do_settings_sections( 'pricode-chatgpt-settings-group' ); ?>
+        <input type="password" name="pricode_chatgpt_api_key" value="<?php echo esc_attr( get_option('pricode_chatgpt_api_key') ); ?>" />
+       <?php submit_button(); ?>
+   </form>
+<?php
+}
+
+function pricode_chatgpt_publish_post_callback(){
+?>
+    <h1>Chat with ChatGPT</h1>
+    <form id="pricode-chatgpt-form">
+    <p><input type="text" class="prompt" name="prompt" id="pricode-chatgpt-prompt" maxlength="200" placeholder='Hi can you please write a blog about...'/></p>
+    <?php wp_nonce_field('pricode_publish_post', 'pricode_publish_post_nonce') ?>
+    <p><button id="publish-post-button">Publish post</button></p>
+    <small id="chatgpt-response"><b>Response:</b> <span></span></small>
+    </form>
+    
+    <script>
+        jQuery('#pricode-chatgpt-form').submit(function(event){
+            event.preventDefault();
+            jQuery('#publish-post-button').attr('disable',true);
+            data = {
+                action: 'pricode_publish_post',
+                data: jQuery('#pricode-chatgpt-form').serialize()
+            }
+            
+            jQuery.post('<?php echo admin_url( 'admin-ajax.php' ) ?>', data, function(response){
+                jQuery('#chatgpt-response span').empty();
+                jQuery('#chatgpt-response span').removeClass('error');
+                jQuery('#chatgpt-response span').removeClass('success');
+                jQuery('#chatgpt-response span').html(response.message);
+                if(response.success){
+                    jQuery('#chatgpt-response span').addClass('success');
+                }else{
+                    jQuery('#chatgpt-response span').addClass('error');
+                }
+                jQuery('#publish-post-button').attr('disable',false);
+            }, 'json');
+        })
+    </script>
+    <style>
+        .error{color: red}
+        .success{color: green}
+        .prompt{width: 90%}
+    </style>
+<?php
+
+}
+add_action('wp_ajax_pricode_publish_post', 'pricode_chatgpt_ajax_response');
+
+function pricode_chatgpt_ajax_response(){
+    parse_str($_POST['data'], $data);
+    if ( ! wp_verify_nonce($data['pricode_publish_post_nonce'], 'pricode_publish_post') ){
+        return wp_send_json( ['success' => false, 'message' => 'Incorrect data'] );    
+    }
+    if( empty( $data['prompt'] ) ) {
+        return wp_send_json( ['success' => false, 'message' => 'empty prompt'] );    
+    }
+
+    $response = pricode_chatgpt_create_post( $data['prompt'] . '. Please use the following json format { title, content: [ { heading, [paragraphs] } ] }' );
+    if( is_wp_error( $response ) ){
+        return wp_send_json( ['success' => false, 'message' => 'Something went wrong'] );    
+    }
+    return wp_send_json( ['success' => true, 'message' => 'Post Created successfully!'] );
+
+}
